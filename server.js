910d20a083461b63e71cf50ee8ac144be3b0fa3a
bwof24 <134535566+bwof24@users.noreply.github.com>
2024-09-04 12:22:47 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const pty = require('node-pty');
-const os = require('os');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,8 +12,7 @@ const wss = new WebSocket.Server({ server });
 app.use(express.static('public'));
 
 wss.on('connection', (ws) => {
-    // Use bash for Linux
-    const shell = 'bash';
+    const shell = process.platform === 'win32' ? 'powershell.exe' : 'bash';
 
     const ptyProcess = pty.spawn(shell, [], {
         name: 'xterm-color',
@@ -27,7 +27,19 @@ wss.on('connection', (ws) => {
     });
 
     ws.on('message', (msg) => {
-        ptyProcess.write(msg);
+        if (msg.startsWith('FILE:')) {
+            const [command, fileName, ...fileContent] = msg.split(':');
+            const filePath = path.join(process.env.HOME, fileName);
+            if (command === 'FILE_CREATE') {
+                fs.writeFileSync(filePath, fileContent.join(':'));
+                ptyProcess.write(`echo "File ${fileName} created." \r\n`);
+            } else if (command === 'FILE_DELETE') {
+                fs.unlinkSync(filePath);
+                ptyProcess.write(`echo "File ${fileName} deleted." \r\n`);
+            }
+        } else {
+            ptyProcess.write(msg);
+        }
     });
 
     ws.on('close', () => {
